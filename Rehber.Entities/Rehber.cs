a9b0f8d2c1e22770c2bdb8cabba46fa4991ef28e
Ahmet Selçuk Tepe <ahmetselcuktepe@hotmail.com>
2022-01-12 22:40:54 +0300

    Proje Oluşturuldu ve Entites Katmanı Tasarlandı.

---
+++ Rehber.Entities/Rehber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Rehber.Entities
+{
+    public class Rehber
+    {
+
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        [StringLength(50)]
+        public string Ad { get; set; }
+
+        [StringLength(50)]
+        public string Soyad { get; set; }
+
+        [StringLength(200)]
+        public string Firma { get; set; }
+
+        public bool Kaldirma { get; set; }
+
+    }
+}
---
+++ Rehber.Entities/RehberDetail.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Rehber.Entities
+{
+   public class RehberDetail
+    {
+        [Key,DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int İd { get; set; }
+        [StringLength(50)]
+        public string Key { get; set; }
+        [StringLength(50)]
+        public string Deger { get; set; }
+        [StringLength(50)]
+        public string KisiId { get; set; }
+
+        public bool Kaldirma { get; set; }
+    }
+}
