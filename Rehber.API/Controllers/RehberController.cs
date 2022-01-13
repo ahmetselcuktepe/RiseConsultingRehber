0d199f5e2b96c331b30c91e4b7a7b1cd5883c116
Ahmet Selçuk Tepe <ahmetselcuktepe@hotmail.com>
2022-01-13 11:26:07 +0300

    API Katmanı Oluşturuldu Interface bağlantıları sağlanıyor

---
+++ Rehber.API/Controllers/RehberController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Rehber.Business.Abstract;
+using Rehber.Business.Concrete;
+using Rehber.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rehber.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RehberController : ControllerBase
+    {
+        private IRehberKService _rehberService;
+
+        public RehberController()
+        {
+            _rehberService = new RehberKManager();
+        }
+
+        [HttpGet]
+        public List<RehberK> Get()
+        {
+            return _rehberService.GetAllRehberKs();
+        }
+    }
+}
